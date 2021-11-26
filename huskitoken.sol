d0a241697d77749596ce4ce7b1ea1424e618d3df
ParanoidSyntaxError <71923508+ParanoidSyntaxError@users.noreply.github.com>
2021-11-27 02:16:28 +1100

    Basic token functionality

--- huskitoken.sol
+++ huskitoken.sol
@@ -1,1 +1,425 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+// Utilities
+library Address 
+{
+    function isContract(address account) internal view returns (bool) 
+    {
+        uint256 size;
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+
+    function sendValue(address payable recipient, uint256 amount) internal 
+    {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        (bool success, ) = recipient.call{ value: amount }("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+
+    function functionCall(address target, bytes memory data) internal returns (bytes memory) 
+    {
+        return functionCall(target, data, "Address: low-level call failed");
+    }
+
+    function functionCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) 
+    {
+        return functionCallWithValue(target, data, 0, errorMessage);
+    }
+
+    function functionCallWithValue(address target, bytes memory data, uint256 value) internal returns (bytes memory) 
+    {
+        return functionCallWithValue(target, data, value, "Address: low-level call with value failed");
+    }
+
+    function functionCallWithValue(address target, bytes memory data, uint256 value, string memory errorMessage) internal returns (bytes memory) 
+    {
+        require(address(this).balance >= value, "Address: insufficient balance for call");
+        require(isContract(target), "Address: call to non-contract");
+
+        (bool success, bytes memory returndata) = target.call{ value: value }(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    function functionStaticCall(address target, bytes memory data) internal view returns (bytes memory) 
+    {
+        return functionStaticCall(target, data, "Address: low-level static call failed");
+    }
+
+    function functionStaticCall(address target, bytes memory data, string memory errorMessage) internal view returns (bytes memory) 
+    {
+        require(isContract(target), "Address: static call to non-contract");
+
+        (bool success, bytes memory returndata) = target.staticcall(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    function functionDelegateCall(address target, bytes memory data) internal returns (bytes memory) 
+    {
+        return functionDelegateCall(target, data, "Address: low-level delegate call failed");
+    }
+
+    function functionDelegateCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) 
+    {
+        require(isContract(target), "Address: delegate call to non-contract");
+
+        (bool success, bytes memory returndata) = target.delegatecall(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    function _verifyCallResult(bool success, bytes memory returndata, string memory errorMessage) private pure returns(bytes memory) 
+    {
+        if (success) 
+        {
+            return returndata;
+        } 
+        else 
+        {
+            if (returndata.length > 0) 
+            {
+                assembly 
+                {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } 
+            else 
+            {
+                revert(errorMessage);
+            }
+        }
+    }
+}
+
+abstract contract Context 
+{
+    function _msgSender() internal view virtual returns (address) 
+    {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) 
+    {
+        this;
+        return msg.data;
+    }
+}
+
+abstract contract Ownable is Context 
+{
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor () 
+    {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    function owner() public view virtual returns (address) 
+    {
+        return _owner;
+    }
+
+    modifier onlyOwner() 
+    {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner 
+    {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner 
+    {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+// ERC20 standards
+interface IERC20 
+{
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+interface IERC20Metadata is IERC20 
+{
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+}
+
+// Huski token
+contract Huski is Context, IERC20, IERC20Metadata 
+{
+    using Address for address;
+
+    event Burn(address indexed from, uint256 value);
+
+    mapping (address => uint256) private _rOwned;
+    mapping (address => uint256) private _tOwned;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    uint256 private _tTotal;
+    uint256 private _rTotal;
+    uint256 private _tFeeTotal;
+    uint256 private constant MAX = ~uint256(0);
+    
+    uint256 public constant _burnTax = 10;
+    uint256 public constant _reflectTax = 10;
+
+    constructor() 
+    {
+        _name = "Huski";
+        _symbol = "HSKI";
+        _decimals = 9;
+
+        _tTotal = (10**9) * (10**_decimals);
+        _rTotal = (MAX - (MAX % _tTotal));
+
+        _rOwned[_msgSender()] = _rTotal;
+    }
+
+    function name() public view virtual override returns (string memory) 
+    {
+        return _name;
+    }
+
+    function symbol() public view virtual override returns (string memory) 
+    {
+        return _symbol;
+    }
+
+    function decimals() public view virtual override returns (uint8) 
+    {
+        return _decimals;
+    }
+
+    function totalSupply() public view override returns (uint256) 
+    {
+        return _tTotal;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) 
+    {
+        return tokenFromReflection(_rOwned[account]);
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) 
+    {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) 
+    {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public virtual override returns (bool) 
+    {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) 
+    {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+
+        unchecked 
+        {
+            _approve(sender, _msgSender(), currentAllowance - amount);
+        }
+
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) 
+    {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) 
+    {
+        uint256 currentAllowance = _allowances[_msgSender()][spender];
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+
+        unchecked 
+        {
+            _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
+    }
+
+    function totalFees() public view returns (uint256) 
+    {
+        return _tFeeTotal;
+    }
+
+    function reflect(uint256 tAmount) public 
+    {
+        address sender = _msgSender();
+        (uint256 rAmount,,,,,) = _getValues(tAmount);
+        _rOwned[sender] = _rOwned[sender] - rAmount;
+        _rTotal = _rTotal - rAmount;
+        _tFeeTotal = _tFeeTotal + tAmount;
+    }
+
+    function reflectionFromToken(uint256 tAmount, bool deductTransferFee) public view returns(uint256) 
+    {
+        require(tAmount <= _tTotal, "Amount must be less than supply");
+        if (!deductTransferFee) 
+        {
+            (uint256 rAmount,,,,,) = _getValues(tAmount);
+            return rAmount;
+        } 
+        else 
+        {
+            (,uint256 rTransferAmount,,,,) = _getValues(tAmount);
+            return rTransferAmount;
+        }
+    }
+
+    function tokenFromReflection(uint256 rAmount) public view returns(uint256) 
+    {
+        require(rAmount <= _rTotal, "Amount must be less than total reflections");
+        uint256 currentRate =  _getRate();
+        return rAmount / currentRate;
+    }
+
+    function _reflectFee(uint256 rFee, uint256 tFee) private 
+    {
+        _rTotal = _rTotal - rFee;
+        _tFeeTotal = _tFeeTotal + tFee;
+    }
+
+    function _getValues(uint256 tAmount) private view returns (uint256, uint256, uint256, uint256, uint256, uint256) 
+    {
+        (uint256 tTransferAmount, uint256 tFee, uint256 tBurn) = _getTValues(tAmount);
+        (uint256 rAmount, uint256 rTransferAmount, uint256 rFee) = _getRValues(tAmount, tFee, tBurn, _getRate());
+        return (rAmount, rTransferAmount, rFee, tTransferAmount, tFee, tBurn);
+    }
+
+    function _getTValues(uint256 tAmount) private pure returns (uint256, uint256, uint256) 
+    {
+        uint256 tFee = (tAmount / 100) * _reflectTax;
+        uint256 tBurn = (tAmount / 100) * _burnTax;
+        uint256 tTransferAmount = tAmount - tFee - tBurn;
+        return (tTransferAmount, tFee, tBurn);
+    }
+
+    function _getRValues(uint256 tAmount, uint256 tFee, uint256 tBurn, uint256 currentRate) private pure returns (uint256, uint256, uint256) 
+    {
+        uint256 rAmount = tAmount * currentRate;
+        uint256 rFee = tFee * currentRate;
+        uint256 rLiquidity = tBurn * currentRate;
+        uint256 rTransferAmount = rAmount - rFee - rLiquidity;
+        return (rAmount, rTransferAmount, rFee);
+    }
+
+    function _getRate() private view returns(uint256) 
+    {
+        (uint256 rSupply, uint256 tSupply) = _getCurrentSupply();
+        return rSupply / tSupply;
+    }
+
+    function _getCurrentSupply() private view returns(uint256, uint256) 
+    {
+        uint256 rSupply = _rTotal;
+        uint256 tSupply = _tTotal;      
+
+        if (rSupply < _rTotal / _tTotal) return (_rTotal, _tTotal);
+        {
+            return (rSupply, tSupply);
+        }
+    }
+
+    // WIP
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual 
+    {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(amount > 0, "Transfer amount must be greater than zero");
+        
+        (uint256 rAmount, uint256 rTransferAmount, uint256 rFee, uint256 tTransferAmount, uint256 tFee, uint256 tBurn) = _getValues(amount);
+        _rOwned[sender] = _rOwned[sender] - rAmount;
+        _rOwned[recipient] = _rOwned[recipient] + rTransferAmount;
+        _burn(sender, tBurn);
+        _reflectFee(rFee, tFee);
+        emit Transfer(sender, recipient, tTransferAmount);
+    }
+
+    // WIP
+    function _burn(address account, uint256 amount) internal virtual 
+    {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        uint256 accountBalance = balanceOf(account);
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+
+        unchecked 
+        {
+            _rOwned[account] = accountBalance - amount;
+        }
+
+        _tTotal -= amount;
+
+        emit Burn(account, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal virtual 
+    {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function burn(uint256 amount) public 
+    {
+        _burn(_msgSender(), amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public 
+    {
+        uint256 currentAllowance = allowance(account, _msgSender());
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        _approve(account, _msgSender(), currentAllowance - amount);
+        _burn(account, amount);
+    }
+}
